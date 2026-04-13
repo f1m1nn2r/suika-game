8d15c4e87658284f8ff576de03d3c02b342fd1c1
김민중 <f1minn2r@naver.com>
2026-04-13 23:03:39 +0900

    feat: matter 초기 세팅, 맵 생성 함수 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
-import { useEffect, useRef } from "react";
+import Matter from "matter-js";
 import "./App.css";
+import { useEffect, useRef } from "react";
+import { createMap } from "./physics/create-map";
+import { initMatter } from "./physics/init-matter";
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
+    if (!containerRef.current || !canvasRef.current) return;
 
+    const canvas = canvasRef.current;
     canvas.width = 700;
     canvas.height = 700;
 
-    console.log(canvas.getBoundingClientRect());
+    const { engine, render } = initMatter({
+      canvas,
+      container: containerRef.current,
+    });
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    createMap({
+      engine,
+      width: 700,
+      height: 700,
+    });
 
-    // 바닥
-    ctx.fillStyle = "#228b22";
-    ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
-
-    // 양쪽 벽
-    ctx.fillStyle = "#A52A2A";
-    ctx.fillRect(0, 0, 20, canvas.height);
-
-    ctx.fillStyle = "#A52A2A";
-    ctx.fillRect(canvas.width - 20, 0, 20, canvas.height);
+    return () => {
+      Matter.Render.stop(render);
+      Matter.Engine.clear(engine);
+    };
   }, []);
 
   return (
-    <main className="main">
-      <section>
-        <canvas ref={canvasRef} className="suika-canvas" id="canvas"></canvas>
-      </section>
-    </main>
+    <div ref={containerRef}>
+      <canvas ref={canvasRef} className="suika-canvas" />
+    </div>
   );
 }
 
